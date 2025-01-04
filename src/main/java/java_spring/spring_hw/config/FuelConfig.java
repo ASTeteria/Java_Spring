250d413911946036ea9cf7eebb083f2d1e10016c
ASTeteria <teteryawushu@gmail.com>
2025-01-04 17:13:27 +0200

    Initial commit

---
+++ src/main/java/java_spring/spring_hw/config/FuelConfig.java
@@ -0,0 +1,16 @@
+package java_spring.spring_hw.config;
+
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+import java.util.Map;
+
+@Data
+@Configuration
+@ConfigurationProperties
+public class FuelConfig {
+    private Map<String, List<String>> fuelTypes;
+}
---
+++ src/main/java/java_spring/spring_hw/controller/ReferenceDataController.java
@@ -0,0 +1,45 @@
+package java_spring.spring_hw.controller;
+
+import java_spring.spring_hw.config.FuelConfig;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/cars")
+@RequiredArgsConstructor
+public class ReferenceDataController {
+
+    @Value("${engine-types}")
+    private List<String> engineTypes;
+
+    private final FuelConfig fuelConfig;
+
+    // engine-types
+    @GetMapping("/engine-types")
+    public List<String> getEngineTypes() {
+        return engineTypes;
+    }
+
+    // fuel-types
+    @GetMapping("/fuel-types")
+    public Map<String, List<String>> getFuelTypes() {
+        return fuelConfig.getFuelTypes();
+    }
+
+    // fuel-types/{fuelName}
+
+    @GetMapping("/fuel-types/{fuelName}")
+    public List<String> getFuelTypeByName(@PathVariable String fuelName) {
+        List<String> options = fuelConfig.getFuelTypes().get(fuelName);
+        if (options == null) {
+            throw new IllegalArgumentException("Тип палива не знайдено: " + fuelName);
+        }
+        return options;
+    }
+
+
+}
