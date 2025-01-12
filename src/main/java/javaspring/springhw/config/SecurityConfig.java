4b592ed5d18400bd9cbde5116faa30a96fa890c0
ASTeteria <teteryawushu@gmail.com>
2025-01-12 08:03:01 +0200

    HW4

---
+++ src/main/java/javaspring/springhw/config/SecurityConfig.java
@@ -0,0 +1,9 @@
+package javaspring.springhw.config;
+
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnabledMethodSecurity
+public class SecurityConfig {
+}
--- src/main/java/javaspring/springhw/entity/Maintenance.java
+++ src/main/java/javaspring/springhw/entity/Maintenance.java
@@ -1,25 +1,5 @@
 package javaspring.springhw.entity;
 
-//import lombok.Data;
-//import org.bson.types.ObjectId;
-//import org.springframework.data.mongodb.core.mapping.Document;
-//import org.springframework.data.mongodb.core.mapping.MongoId;
-//
-//@Document("maintenance-review")
-//@Data
-//public class Maintenance {
-//
-//    @MongoId
-//    private ObjectId id;
-//
-//    private String name;
-//
-//    private String description;
-//
-//    private Double price;
-//}
-
-import jakarta.persistence.Id;
 import lombok.Builder;
 import lombok.Data;
 import org.bson.types.ObjectId;
